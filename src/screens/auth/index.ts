66fbe28b8a399ad7349a71b0f3e2ccc388c6d321
gk <gkpattanaik001@gmail.com>
2024-04-14 00:39:24 +0530

    error fixed

--- src/screens/auth/index.ts
+++ src/screens/auth/index.ts
@@ -1,5 +1,5 @@
 export {default as Login} from './Login';
 export {default as Register} from './Register';
 export {default as ResetPassword} from './ResetPassword';
-export {default as forgotPassword} from './forgotPassword';
+export {default as ForgotPassword} from './ForgotPassword';
 export {default as OtpScreen} from './OtpScreen';
