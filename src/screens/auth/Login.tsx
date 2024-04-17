3d578f23ce34ce5f27e0f1444e737dca060a5e83
chinmayee behera <chinmayeebehera2020@gmail.com>
2024-04-17 09:48:31 +0530

    changes in login

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {
   Box,
   Heading,
@@ -10,21 +10,21 @@ import {
   Spinner,
 } from '@gluestack-ui/themed';
 
-import React, { useMemo, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useWindowDimensions } from 'react-native';
+import React, {useMemo, useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {useWindowDimensions} from 'react-native';
 import useAuth from '../../hooks/useAuth';
-import { useToast } from '@gluestack-ui/themed';
-import { PublicRouteProps } from '../../routes/public/types';
-import AppIcon, { IconProps } from '../../components/core/AppIcon';
+import {useToast} from '@gluestack-ui/themed';
+import {PublicRouteProps} from '../../routes/public/types';
+import AppIcon, {IconProps} from '../../components/core/AppIcon';
 import useMutation from '../../hooks/useMutation';
 import useSwrApi from '../../hooks/useSwrApi';
-import { Alert } from '@gluestack-ui/themed';
-import { IMAGES } from '../../assets';
-import { COLORS } from '../../styles';
+import {Alert} from '@gluestack-ui/themed';
+import {IMAGES} from '../../assets';
+import {COLORS} from '../../styles';
 import AppInput from '../../components/core/AppInput';
 import Btn from '../../components/core/Btn';
-import { Toast } from '@gluestack-ui/themed';
+import {Toast} from '@gluestack-ui/themed';
 
 type FormInput = {
   key: string;
@@ -55,33 +55,31 @@ export default function Register(): JSX.Element {
   const [isPhone, setIsPhone] = useState(false);
   const [secureTextEntry, setSecureTextEntry] = useState(true);
   const [visible, setVisible] = useState(false);
-  const { navigate } = useNavigation<PublicRouteProps>();
-  const { height } = useWindowDimensions();
+  const {navigate} = useNavigation<PublicRouteProps>();
+  const {height} = useWindowDimensions();
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
   } = useForm<FormData>();
 
-
-  const { setUser, getUser, setToken } = useAuth();
-  const { mutation: login, isLoading } = useMutation();
-  const { mutation: gmail, isLoading: isGmailValidating } = useMutation();
-  const { mutation: gLogin } = useMutation();
+  const {setUser, getUser, setToken} = useAuth();
+  const {mutation: login, isLoading} = useMutation();
+  const {mutation: gmail, isLoading: isGmailValidating} = useMutation();
+  const {mutation: gLogin} = useMutation();
   let objData: any = {};
-  const { data, error, isValidating } = useSwrApi(`auth/google/select-profile`);
+  const {data, error, isValidating} = useSwrApi(`auth/google/select-profile`);
 
   // console.log(data, 'data');
 
   const handleLogin = async (formData: FormData) => {
     try {
-      const { mobile } = formData;
+      const {mobile} = formData;
       // console.log('Mobile Number:', mobile);
       // console.log('Mobile Number:', selectedCountry.name);
       // console.log('Mobile Number:', selectedCountry.phone);
       objData = {
         phone: mobile,
-
       };
       const res = await login(`auth/register-with-phone`, {
         isAlert: true,
@@ -109,9 +107,9 @@ export default function Register(): JSX.Element {
   };
 
   const handleLoginWithGmail = async (data: FormData) => {
-    const { username, password } = data;
+    const {username, password} = data;
     try {
-      let objData = { email: username, password: password };
+      let objData = {email: username, password: password};
       const res = await gmail(`auth/register-with-email-and-password`, {
         body: {
           email: username,
@@ -119,17 +117,21 @@ export default function Register(): JSX.Element {
         },
       });
       console.log(res);
-      if (res?.results?.success && res?.results?.data?.token && res?.results?.data?.otp) {
+      if (
+        res?.results?.success &&
+        res?.results?.data?.token &&
+        res?.results?.data?.otp
+      ) {
         setToken(res?.results?.data?.token);
         await gLogin(`auth/verify-email-or-phone`, {
           body: {
             token: res?.results?.data?.token,
             otp: res?.results?.data?.otp,
           },
-        })
+        });
         toast.show({
           title: res?.results?.success
-            ? 'Register Successful login your account !'
+            ? 'Register Successful login your account !!!!!'
             : 'Login Failed',
           duration: 5000,
         });
@@ -143,14 +145,13 @@ export default function Register(): JSX.Element {
     }
   };
 
-
   const formInputs1: FormInput[] = useMemo(
     () => [
       {
         key: 'mobile',
         label: undefined,
         placeholder: 'Enter your mobile number',
-        icon: { IoniconsName: 'call-outline', color: 'gray' },
+        icon: {IoniconsName: 'call-outline', color: 'gray'},
         rules: {
           required: 'Mobile number is required',
           pattern: {
@@ -168,7 +169,7 @@ export default function Register(): JSX.Element {
         key: 'E mail',
         label: undefined,
         placeholder: 'Enter your email address',
-        icon: { FeatherName: 'mail' },
+        icon: {FeatherName: 'mail'},
         rules: {
           required: 'Username is required',
           pattern: {
@@ -186,7 +187,7 @@ export default function Register(): JSX.Element {
         key: 'password',
         label: undefined,
         placeholder: 'Password',
-        icon: { FeatherName: 'lock' },
+        icon: {FeatherName: 'lock'},
         rules: {
           required: 'Password is required',
           minLength: {
@@ -199,7 +200,7 @@ export default function Register(): JSX.Element {
           rightElement: (
             <Btn
               colors={['#fff', '#fff']}
-              _text={{ color: 'black', fontSize: '$xs' }}
+              _text={{color: 'black', fontSize: '$xs'}}
               onPress={() => setSecureTextEntry(!secureTextEntry)}>
               {secureTextEntry ? 'Show' : 'Hide'}
             </Btn>
@@ -210,10 +211,13 @@ export default function Register(): JSX.Element {
         key: 'ConfirmPassword',
         label: undefined,
         placeholder: 'Confirm Password',
-        icon: { FeatherName: 'lock' },
+        icon: {FeatherName: 'lock'},
         rules: {
           required: 'Password is required',
-          confirmPassword: { value: 'password', message: 'Password does not match' },
+          confirmPassword: {
+            value: 'password',
+            message: 'Password does not match',
+          },
           minLength: {
             value: 6,
             message: 'Password must be at least 6 characters long',
@@ -224,7 +228,7 @@ export default function Register(): JSX.Element {
           rightElement: (
             <Btn
               colors={['#fff', '#fff']}
-              _text={{ color: 'black', fontSize: '$xs' }}
+              _text={{color: 'black', fontSize: '$xs'}}
               onPress={() => setSecureTextEntry(!secureTextEntry)}>
               {secureTextEntry ? 'Show' : 'Hide'}
             </Btn>
@@ -235,21 +239,17 @@ export default function Register(): JSX.Element {
     [secureTextEntry],
   );
 
-
   return (
     <>
-      <Box
-        bg={'white'}
-        h={'100%'}
-      >
+      <Box bg={'white'} h={'100%'}>
         <Image
           w="$56"
           h="$56"
           source={IMAGES.ONBOARDING}
           alt="logo"
-          alignSelf='center'
+          alignSelf="center"
         />
-        <VStack space='md' justifyContent={'space-between'} h={'50%'}>
+        <VStack space="md" justifyContent={'space-between'} h={'50%'}>
           <Box mx={'$4'}>
             <Heading fontSize={18} color={'black'}>
               Register with your us
@@ -260,7 +260,6 @@ export default function Register(): JSX.Element {
               </Text>
             </Box>
           </Box>
-
 
           <VStack space={'md'}>
             <Box px="$4">
@@ -274,40 +273,29 @@ export default function Register(): JSX.Element {
               ))}
             </Box>
 
-            <Box mx={15} >
+            <Box mx={15}>
               <Btn
                 bg={COLORS.theme[600]}
-                _text={{ color: 'white', fontSize: '$sm' }}
-                onPress={handleSubmit(handleLoginWithGmail)}
-              >
+                _text={{color: 'white', fontSize: '$sm'}}
+                onPress={handleSubmit(handleLoginWithGmail)}>
                 <Heading fontSize={15} color={'white'}>
                   {isLoading || isGmailValidating ? (
                     <Spinner size={'small'} color={'white'} />
                   ) : (
                     <HStack alignItems={'center'} space={'xs'}>
-                      <Text color={'white'} bold> Submit </Text>
-                      <AppIcon
-                        FeatherName="log-in"
-                        color={'white'}
-                        size={20}
-                      />
+                      <Text color={'white'} bold>
+                        {' '}
+                        Submit{' '}
+                      </Text>
+                      <AppIcon FeatherName="log-in" color={'white'} size={20} />
                     </HStack>
                   )}
                 </Heading>
               </Btn>
-
             </Box>
-
-
           </VStack>
 
-
-
-          <HStack
-            alignItems="center"
-            flexDirection="row"
-            mx={'$5'}
-          >
+          <HStack alignItems="center" flexDirection="row" mx={'$5'}>
             <Text fontSize="$xs" fontWeight="400">
               Already Have an account?
             </Text>
@@ -318,26 +306,22 @@ export default function Register(): JSX.Element {
                 fontSize: '$xs',
                 bold: true,
               }}
-              onPress={() => navigate('Login', { objData })}>
+              onPress={() => navigate('Login', {objData})}>
               Login
             </Btn>
           </HStack>
-
-
 
           <>
             <Text mb={2} textAlign={'center'}>
               - Or -
             </Text>
 
-
             <Pressable
-              $pressed={{ opacity: 0.8 }}
+              $pressed={{opacity: 0.8}}
               w={'92%'}
               py={1.5}
               borderColor={'blue.800'}
               rounded={'$full'}
-
               bg={'white'}
               alignSelf={'center'}
               justifyContent={'center'}
@@ -359,44 +343,33 @@ export default function Register(): JSX.Element {
                   Register With Google
                 </Heading>
               </HStack>
-            </Pressable></>
-
-
+            </Pressable>
+          </>
 
           <Box alignItems={'center'}>
             <HStack>
-              <Text fontSize={11} color="gray.500" >
+              <Text fontSize={11} color="gray.500">
                 By continuing, you agree to our{' '}
               </Text>
-              <Pressable
-                $pressed={{ opacity: 0.6 }}
-              >
-                <Text
-                  fontSize={12}
-                  color="red.500"
-                  underline>
+              <Pressable $pressed={{opacity: 0.6}}>
+                <Text fontSize={12} color="red.500" underline>
                   Terms and Conditions
                 </Text>
               </Pressable>
             </HStack>
             <HStack>
-              <Text fontSize={11} color="gray.500" >
+              <Text fontSize={11} color="gray.500">
                 and{' '}
               </Text>
-              <Pressable
-                $pressed={{ opacity: 0.6 }}
-              >
-                <Text
-                  fontSize={12}
-                  color="red.500"
-                  underline>
+              <Pressable $pressed={{opacity: 0.6}}>
+                <Text fontSize={12} color="red.500" underline>
                   Privacy Policy
                 </Text>
               </Pressable>
             </HStack>
           </Box>
         </VStack>
-      </Box >
+      </Box>
     </>
   );
 }
