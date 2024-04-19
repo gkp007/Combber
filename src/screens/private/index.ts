22e5b61a2ea96fc18ea86d008bc1b7af0992eb45
Debabrata Giri <debabratagiri5525@gmail.com>
2024-04-19 15:22:07 +0530

    added new map page & update ui

--- src/screens/private/index.ts
+++ src/screens/private/index.ts
@@ -2,3 +2,4 @@ export {default as Home} from './Home';
 export {default as Profile} from './Profile';
 export {default as Notification} from './Notification';
 export {default as BusinessInfo} from './OwnerOnboarding/BusinessInfo';
+export {default as GooMap} from '../private/googleMap/GooMap';
