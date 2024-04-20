a55e9c7f9ac2bc18e30493828ee2fe3caec495d7
gk <gkpattanaik001@gmail.com>
2024-04-21 00:12:06 +0530

    Allbookings card completed

--- src/screens/private/index.ts
+++ src/screens/private/index.ts
@@ -1,4 +1,5 @@
 export {default as Home} from './Home';
 export {default as Profile} from './Profile';
 export {default as Notification} from './Notification';
+export {default as AllBookings} from './AllBookings';
 export {default as BusinessInfo} from './OwnerOnboarding/BusinessInfo';
