b5dd6be12d220b8b1681ad1fe1fed10fc3077556
gk <gkpattanaik001@gmail.com>
2024-04-26 16:43:08 +0530

    line graph

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -1,50 +1,88 @@
 import React from 'react';
-import { Box } from '@gluestack-ui/themed';
+import { Box, Text } from '@gluestack-ui/themed';
 import { HEIGHT } from '~/styles';
 import { LineChart } from 'react-native-gifted-charts';
 
 const Home = () => {
   const d1 = [
-    { value: 110, label: 'Mon' },
-    { value: 90, label: 'Tue' },
-    { value: 100, label: 'Wed' },
-    { value: 120, label: 'Thu' },
-    { value: 100, label: 'Fri' },
-    { value: 80, label: 'Sat' },
-    { value: 90, label: 'Sun' },
-
+    { value: 110, label: 'M' },
+    { value: 70, label: 'T' },
+    { value: 100, label: 'W' },
+    { value: 120, label: 'T' },
+    { value: 30, label: 'F' },
+    { value: 120, label: 'S' },
+    { value: 90, label: 'S' },
   ];
 
   return (
-    <Box height={HEIGHT * 0.8}>
-      <LineChart
-        isAnimated
-        curved
-        thickness={2}
-        animateOnDataChange
-        animationDuration={1000}
-        onDataChangeAnimationDuration={300}
-        areaChart
-        yAxisTextStyle={{ color: 'black' }}
-        startFillColor={'rgb(8,10,234)'}
-        endFillColor={'white'}
-        startOpacity={0.2}
-        endOpacity={0.1}
-        backgroundColor="transparent"
-        initialSpacing={10}
-        data={d1}
-        maxValue={140}
-        noOfSections={7}
-        spacing={60}
-        hideDataPoints
-        hideRules
-        color="blue"
-        yAxisColor={'transparent'}
-        xAxisColor={'transparent'}
-        xAxisLabelTextStyle={{ width: 80, marginLeft: -36 }}
+    <>
+      <Box m={2} bg={'white'} mt={'$5'} softShadow='1' mx={'$1.5'} py={'$5'} borderRadius={'$2xl'} py={5}>
+        <LineChart
+          isAnimated
+          curved
+          thickness={1}
+          animateOnDataChange
+          animationDuration={1000}
+          onDataChangeAnimationDuration={300}
+          areaChart
+          yAxisTextStyle={{ color: 'gray' }}
+          startFillColor={'rgb(8,10,254)'}
+          endFillColor={'white'}
+          startOpacity={0.13}
+          endOpacity={0.08}
+          showDataPointOnFocus
+          showStripOnFocus
+          showTextOnFocus
+          focusEnabled
+          backgroundColor="transparent"
+          initialSpacing={0}
+          data={d1}
+          maxValue={140}
+          noOfSections={3}
+          spacing={60}
+          hideDataPoints
+          hideRules
+          color="blue"
+          yAxisColor={'transparent'}
+          xAxisColor={'transparent'}
+          xAxisLabelTextStyle={{ width: 80, marginLeft: -36 }}
 
-      />
-    </Box>
+          pointerConfig={{
+            pointerStripHeight: 175,
+            pointerStripColor: 'blue',
+            pointerStripWidth: 1,
+            pointerColor: 'lightgray',
+            radius: 6,
+            strokeDashArray: [2, 5],
+            pointerLabelWidth: 100,
+            pointerLabelHeight: 90,
+            activatePointersOnLongPress: true,
+            autoAdjustPointerLabelPosition: false,
+            pointerLabelComponent: items => {
+              return (
+                <Box
+                  style={{
+                    height: 90,
+                    width: 100,
+                    justifyContent: 'center',
+                    marginTop: -30,
+                    marginLeft: -40,
+                  }}>
+                  <Text style={{ color: 'white', fontSize: 14, marginBottom: 6, textAlign: 'center' }}>
+                    {items[0].date}
+                  </Text>
+                  <Box style={{ paddingHorizontal: 10, paddingVertical: 6, borderRadius: 16, backgroundColor: 'gray' }}>
+                    <Text style={{ fontWeight: 'bold', textAlign: 'center', color: 'white' }}>
+                      {'₹ ' + items[0].value + '.0'}
+                    </Text>
+                  </Box>
+                </Box>
+              );
+            },
+          }}
+        />
+      </Box>
+    </>
   );
 };
 
